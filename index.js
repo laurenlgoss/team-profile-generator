1dbda4d8bf684fe3668b88df0cbfecea66c664f4
Lauren Goss <laurenlgoss98@gmail.com>
2021-08-13 15:19:34 -0400

    required necessary packages/files

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
